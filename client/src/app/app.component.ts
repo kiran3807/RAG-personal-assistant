57b71c14a141db90a934173da4014ad5482dd9c8
Kiran Yallabandi <kiranyallabandi@gmail.com>
2024-07-05 04:42:32 +0530

    Feat: Created all basic components and basic visual layout of the client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChatDisplayComponent } from './chat-display/chat-display.component';
+import { ChatInputComponent } from './chat-input/chat-input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ChatDisplayComponent],
+  imports: [RouterOutlet, ChatDisplayComponent, ChatInputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
